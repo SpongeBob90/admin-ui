94c761063d1d2fc640edd76f72c78f09e3d04f8e
SpongeBob90 <1442772080@qq.com>
2020-12-31 11:07:12 +0800

    角色管理主模块布局

--- src/views/role-manage/role-list.vue
+++ src/views/role-manage/role-list.vue
@@ -1,13 +1,29 @@
 <template>
+  <div id="role-manage" class="common-root" :style="{minHeight: minHeight + 'px'}">
+    <!-- 条件筛选查询 -->
+    <div class="select-container">
 
+    </div>
+    <!-- 角色列表 -->
+    <div class="table-container">
+
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "role-list"
+  export default {
+    data() {
+      return {
+        minHeight: null
+      }
+    },
+    created() {
+      this.minHeight = window.innerHeight - 92
     }
+  }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  @import "../../styles/list";
 </style>
--- src/views/user-manage/user-list.vue
+++ src/views/user-manage/user-list.vue
@@ -1,13 +1,29 @@
 <template>
+  <div id="user-manage" class="common-root" :style="{minHeight: minHeight + 'px'}">
+    <!-- 条件筛选查询 -->
+    <div class="select-container">
 
+    </div>
+    <!-- 用户列表 -->
+    <div class="table-container">
+
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "user-list"
+  export default {
+    data() {
+      return {
+        minHeight: null
+      }
+    },
+    created() {
+      this.minHeight = window.innerHeight - 92;
     }
+  }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  @import "../../styles/list";
 </style>
